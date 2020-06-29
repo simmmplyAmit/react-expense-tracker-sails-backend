866fbea11f1ad3364ba1aa158461fcfaae9f30a0
Amit Vyas <simmmplyamit@gmail.com>
2020-06-29 14:26:50 +0530

    initial commit

---
+++ api/models/Expense.js
@@ -0,0 +1,39 @@
+/**
+ * Expense.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    title: {
+      type: "string",
+      required: true,
+    },
+    amount: {
+      type: "number",
+      required: true,
+    },
+    income: {
+      type: "boolean",
+      defaultsTo: false,
+    },
+    date: {
+      type: "ref",
+      columnType: "date",
+      defaultsTo: new Date(),
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+  },
+};
