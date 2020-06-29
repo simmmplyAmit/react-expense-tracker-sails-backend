0b1ec60badcd9b00c2a43493b7620d5d989ecd1f
Amit Vyas <simmmplyamit@gmail.com>
2020-06-29 18:46:58 +0530

    production setting change

--- config/env/production.js
+++ config/env/production.js
@@ -344,9 +344,8 @@ module.exports = {
    *                                                                         *
    ***************************************************************************/
   custom: {
-    baseUrl: "https://example.com",
-    internalEmailAddress: "support@example.com",
-
+    // baseUrl: "https://example.com",
+    // internalEmailAddress: "support@example.com",
     // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
     // stripeSecret: 'sk_prod__fake_Nfgh82401348jaDa3lkZ0d9Hm',
     //--------------------------------------------------------------------------
